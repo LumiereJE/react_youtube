f22a1a3372e3e608982860415dafe0c2fbb05dec
lumiereJE <lumiere_remplir@naver.com>
2024-08-17 00:53:19 +0900

    페이지 SEO작업, 설명 및 타이틀 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Main from "../components/section/Main";
 
 const Home = () => {
-  return <Main>Home</Main>;
+  return (
+    <Main title="react Youtube" description="리액트 공부용 페이지입니다.">
+      Home
+    </Main>
+  );
 };
 
 export default Home;
