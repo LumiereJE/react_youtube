08ddc6184648093ab42b714f80ce5ee1d5621fd3
lumiereJE <lumiere_remplir@naver.com>
2024-08-16 23:58:20 +0900

    컴포넌트 비동기 작업/ suspense, lazy

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Main from "../components/section/Main";
 
 const Website = () => {
-  return <div>Website</div>;
+  return <Main>Wdbsite</Main>;
 };
 
 export default Website;
